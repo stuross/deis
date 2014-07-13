ea019175ffdf0c6a01a506a22847bed96e4d8071
Matthew Fisher <matthewf@opdemand.com>
2014-07-13 11:22:38 -0700

    fix(tests/unit): convert to tabs

--- tests/unit/style_guide_test.go
+++ tests/unit/style_guide_test.go
@@ -2,24 +2,24 @@
 package unit
 
 import (
-    "fmt"
-    "os/exec"
-    "regexp"
-    "strings"
-    "testing"
+	"fmt"
+	"os/exec"
+	"regexp"
+	"strings"
+	"testing"
 )
 
 const (
-    COMMIT_SUBJECT_MAX_LINE_LENGTH int = 50
-    COMMIT_MESSAGE_MAX_LINE_LENGTH int = 72
+	COMMIT_SUBJECT_MAX_LINE_LENGTH int = 50
+	COMMIT_MESSAGE_MAX_LINE_LENGTH int = 72
 )
 
 type CommitMessage struct {
-    Message string
+	Message string
 }
 
 func (c CommitMessage) Summary() string {
-    return strings.Split(c.Message, "\n")[0]
+	return strings.Split(c.Message, "\n")[0]
 }
 
 /*
@@ -34,33 +34,33 @@ Knowledge on retrieving each commit from `git log` using ASCII field/record sepa
 came from http://blog.lost-theory.org/post/how-to-parse-git-log-output/
 */
 func TestCommitsAdhereToCommitStyleGuide(t *testing.T) {
-    var validCommitSubject = regexp.MustCompile(`^feat\(.+\): [0-9a-z].+[^.]$|^fix\(.+\): [0-9a-z].+[^.]$|^docs\(.+\): [0-9a-z].+[^.]$|^style\(.+\): [0-9a-z].+[^.]$|^ref\(.+\): [0-9a-z].+[^.]$|^test\(.+\): [0-9a-z].+[^.]$|^chore\(.+\): [0-9a-z].+[^.]$`)
+	var validCommitSubject = regexp.MustCompile(`^feat\(.+\): [0-9a-z].+[^.]$|^fix\(.+\): [0-9a-z].+[^.]$|^docs\(.+\): [0-9a-z].+[^.]$|^style\(.+\): [0-9a-z].+[^.]$|^ref\(.+\): [0-9a-z].+[^.]$|^test\(.+\): [0-9a-z].+[^.]$|^chore\(.+\): [0-9a-z].+[^.]$`)
 
-    cmd := exec.Command("git", "log", "--no-merges", "--format=%s%n%b%x1e", "--after", "2014-07-11")
-    out, err := cmd.Output()
-    if err != nil {
-        t.Fatalf(err.Error())
-    }
-    logs := string(out)
-    for _, row := range strings.Split(strings.Trim(logs, "\n\x1e"), "\x1e") {
-        commit := CommitMessage{Message: strings.Trim(row, "\n")}
-        if !validCommitSubject.MatchString(commit.Summary()) {
-            t.Errorf(fmt.Sprintf("not a valid subject: %v", commit.Message))
-        }
-        if len(commit.Summary()) > COMMIT_SUBJECT_MAX_LINE_LENGTH {
-            t.Errorf(fmt.Sprintf(
-                "subject cannot be longer than %d characters in length: %s",
-                COMMIT_SUBJECT_MAX_LINE_LENGTH,
-                commit.Summary()))
-        }
-        for _, line := range strings.Split(commit.Message, "\n") {
-            if len(line) > COMMIT_MESSAGE_MAX_LINE_LENGTH {
-                t.Errorf(fmt.Sprintf(
-                    "lines cannot be longer than %v characters in length: %s",
-                    COMMIT_MESSAGE_MAX_LINE_LENGTH,
-                    commit.Summary()))
-                break
-            }
-        }
-    }
+	cmd := exec.Command("git", "log", "--no-merges", "--format=%s%n%b%x1e", "--after", "2014-07-11")
+	out, err := cmd.Output()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	logs := string(out)
+	for _, row := range strings.Split(strings.Trim(logs, "\n\x1e"), "\x1e") {
+		commit := CommitMessage{Message: strings.Trim(row, "\n")}
+		if !validCommitSubject.MatchString(commit.Summary()) {
+			t.Errorf(fmt.Sprintf("not a valid subject: %v", commit.Message))
+		}
+		if len(commit.Summary()) > COMMIT_SUBJECT_MAX_LINE_LENGTH {
+			t.Errorf(fmt.Sprintf(
+				"subject cannot be longer than %d characters in length: %s",
+				COMMIT_SUBJECT_MAX_LINE_LENGTH,
+				commit.Summary()))
+		}
+		for _, line := range strings.Split(commit.Message, "\n") {
+			if len(line) > COMMIT_MESSAGE_MAX_LINE_LENGTH {
+				t.Errorf(fmt.Sprintf(
+					"lines cannot be longer than %v characters in length: %s",
+					COMMIT_MESSAGE_MAX_LINE_LENGTH,
+					commit.Summary()))
+				break
+			}
+		}
+	}
 }
